ec7fc75f5956c7375e427bb2f6943e61d61da73b
David Wehr <dawehr@iastate.edu>
2017-12-12 16:02:27 -0600

    Better picture and alignment for static tracking.

--- StructuralAnalysisAR/StaticARManager.mm
+++ StructuralAnalysisAR/StaticARManager.mm
@@ -12,11 +12,11 @@
 #import <MetalKit/MetalKit.h>
 
 StaticARManager::StaticARManager(UIView* view, SCNScene* scene) {
-    UIImage* bgImage = [UIImage imageNamed:@"skywalk_1920_back.png"];
-//    int img_width = bgImage.size.width;
-//    int img_height = bgImage.size.height;
+    UIImage* bgImage = [UIImage imageNamed:@"skywalk_south_far.jpg"];
+    int img_width = bgImage.size.width;
+    int img_height = bgImage.size.height;
     
-//    MTLTextureDescriptor* texDescription = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm width:img_width height:img_height mipmapped:NO];
+    // Load image into texture
     id<MTLDevice> gpu = MTLCreateSystemDefaultDevice();
     MTKTextureLoader* texLoader = [[MTKTextureLoader alloc] initWithDevice:gpu];
     // Set as sRGB to be correct color
@@ -26,18 +26,34 @@ StaticARManager::StaticARManager(UIView* view, SCNScene* scene) {
     if (error) {
         printf("Failed to load static background image\n");
     }
-    
+    // Set texture as background
     scene.background.contents = staticBgTex;
     
+    // Calculate scaling so image is not stretched
+    float aspectImage = (float)img_width / img_height;
+    CGSize viewSize = view.frame.size;
+    float aspectScreen = (float)viewSize.width / viewSize.height;
+    float xScale, yScale;
+    xScale = yScale = 1;
+    if (aspectImage > aspectScreen) {
+        xScale = aspectScreen / aspectImage;
+    }
+    else {
+        yScale = aspectImage / aspectScreen;
+    }
+    GLKMatrix4 bgImgScale = GLKMatrix4MakeScale(xScale, yScale, 1);
+    scene.background.contentsTransform = SCNMatrix4FromGLKMatrix4(bgImgScale);
     
-    float aspectScreen = (float)view.frame.size.width / view.frame.size.height;
+    
     projectionMatrix = GLKMatrix4MakePerspective(36.909 * (M_PI / 180.0), aspectScreen, 0.1, 500);
     
-    cameraMatrix = GLKMatrix4Make(
-                                  -0.987822, -0.009307, 0.155310, 0.000000,
-                                  -0.045182, 0.981796, -0.184486, 0.000000,
-                                  -0.154240, -0.189710, -0.969649, 0.000000,
-                                  -8.753870, -31.452150, -204.253311, 1.000000);
+    GLKMatrix4 trans_mat = GLKMatrix4MakeTranslation(-15, 7, 280);
+    GLKMatrix4 rot_y_mat = GLKMatrix4MakeYRotation(M_PI);
+    GLKMatrix4 rot_x_mat = GLKMatrix4MakeXRotation(0.1);
+    GLKMatrix4 rot_z_mat = GLKMatrix4MakeZRotation(0.02);
+    // Rotate by Y, then X
+    GLKMatrix4 rot_mat = GLKMatrix4Multiply(rot_z_mat, GLKMatrix4Multiply(rot_y_mat, rot_x_mat));
+    cameraMatrix = GLKMatrix4Multiply(rot_mat, trans_mat);
 }
 
 
